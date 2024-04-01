1c7209f0d88b6c73e2588d05d6d624fc6ca6e4bb
MtJakhar <thvsd.10fingers@gmail.com>
2024-04-01 16:26:21 -0400

    dowhile

--- f_section9/example.cpp
+++ f_section9/example.cpp
@@ -1,16 +1,29 @@
 #include <iostream> 
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
-  int scores[] {10,20,30};
+  // int scores[] {10,20,30};
 
   //kind of like for each loop
-  for(auto score: scores) {
-    cout << score << endl;
+  // for(auto score: scores) {
+  //   cout << score << endl;
+  // }
+
+  vector<double> temperatures {87.9, 77.9, 80.0, 72.5};
+  double average_temp {};
+  double total {};
+  
+  if(temperatures.size() != 0) {
+    average_temp = total/temperatures.size();
   }
 
+  cout << fixed << setprecision(1);
+  cout << "Average temperatue is " << average_temp << endl;
+   
   cout << endl;
+
   return 0;
 }
