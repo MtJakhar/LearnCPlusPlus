3574cb683791eff1424dedf1c79a27373858ef40
MtJakhar <thvsd.10fingers@gmail.com>
2024-04-09 13:13:32 -0400

    fixed program to take sentences

--- g_section10/challenge.cpp
+++ g_section10/challenge.cpp
@@ -5,45 +5,40 @@ using namespace std;
 
 int main () {
   string word{}; 
-  string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"}; 
-  string key {"XYZABCDEFGHIJKLMNOPQRSTUVWxyzabcdefghijklmnopqrstuvw"}; 
+  string alphabet {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+,./;'[]\\<>?:{}|-= "}; 
+  string key {"XYZABCDEFGHIJKLMNOPQRSTUVWxyzabcdefghijklmnopqrstuvw!@#$%^&*()_+,./;'[]\\<>?:{}|-= "}; 
   string formattedWord{};
 
-  cout << "This is an encryption program, that uses the Caesarian cypher to encrypt or decrypt your text.\n\nAre you encrypting (e) or decrypting (d)?.\n"; 
+  cout << "This is an encryption program, that uses the Caesarian cypher to encrypt and then decrypt your text.\n\n"; 
 
-  char answer {};
-  cin >> answer; 
+  cout << "Please enter word that is to be encrypted.\n";
 
-  if(answer == 'e') {
-    cout << "Please enter word that is to be encrypted.\n";
-    cin >> word;
+  getline(cin, word);
 
-    for(char letter : word) {
-      formattedWord += key.at(alphabet.find(letter));
-    }
+  for(char letter : word) {
+    formattedWord += key.at(alphabet.find(letter));
+  }
 
-    if(formattedWord.length() > 0) {
-      cout << "Here is your encrypted message: \n" << formattedWord << endl;
-    } else {
-      cout << word << endl;
-      cout << "string is empty" << endl;
-    }
-  } else if (answer == 'd') {
-    cout << "Please enter word that is to be decrypted.\n";
-    cin >> word; 
-
-    for(char letter : word) {
-      formattedWord += alphabet.at(key.find(letter));
-    }
-
-    if(formattedWord.length() > 0) {
-      cout << "Here is your decrypted message: \n" << formattedWord << endl;
-    } else {
-      cout << word << endl;
-      cout << "string is empty" << endl;
-    }
+  if(formattedWord.length() > 0) {
+    cout << "Here is your encrypted message: \n" << formattedWord << endl;
   } else {
-    cout<< "your response is invalid" << endl;
+    cout << word << endl;
+    cout << "string is empty" << endl;
+  }
+  formattedWord.clear();
+  word.clear();
+  cout << "Please enter word that is to be decrypted.\n";
+  getline(cin, word);
+
+  for(char letter : word) {
+    formattedWord += alphabet.at(key.find(letter));
+  }
+
+  if(formattedWord.length() > 0) {
+    cout << "Here is your decrypted message: \n" << formattedWord << endl;
+  } else {
+    cout << word << endl;
+    cout << "string is empty" << endl;
   }
 
   return 0;
