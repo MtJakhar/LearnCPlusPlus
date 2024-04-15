30ea1f492a492c039a925ebe9ccf07f483983223
MtJakhar <thvsd.10fingers@gmail.com>
2024-04-14 20:34:14 -0400

    pyramid challenge complete

--- g_section10/challengeAssignment.cpp
+++ g_section10/challengeAssignment.cpp
@@ -7,9 +7,22 @@ int main () {
   std::string print {};
   std::cin >> word;
 
+  //first create loop which targets current index of string
   for(int i = 0; i < word.length(); i++) {
-    
-    std::cout << word[i] << std::endl;
+    //to create spaces to make pyramid add number of spaces of equal currentIndex - the length of the word; 
+    for(int currentSpaceCount = i; currentSpaceCount < word.length(); currentSpaceCount++) {
+      print.push_back(' ');
+    }
+    //to add left side letters until it reaches currentIndex
+    for(int leftSideIndex = 0; leftSideIndex < i; leftSideIndex++){
+      print.push_back(word.at(leftSideIndex));
+    }
+    //to add right side letters in reverse start at currentIndex and print until after the first index has been printed
+    for(int rightSideIndex = i; rightSideIndex >= 0; rightSideIndex--){
+      print.push_back(word.at(rightSideIndex));
+    }
+    std::cout << print << std::endl;
+    print.clear();
   }   
 
   return 0;
