b57e734f37a00682c7ce6ba6681c4609269003c9
MtJakhar <thvsd.10fingers@gmail.com>
2024-04-23 10:37:52 -0500

    fixed note errors

--- j_section13/move.cpp
+++ j_section13/move.cpp
@@ -12,6 +12,18 @@ class Move {
   //Copy Constructor
   Move(const Move &source);
   //Move Constructor
+  /* The move constructors syntax is as follows
+    Type(Type &&source) || Type::Type(Type &&source) if define outside of class
+    Move(Move &&source) : data(source.data) {
+      source.data = nullptr;
+    }
+    steal the data and then null out the source pointer
+
+    in main function
+    Vector<Move> vec;
+    vec.push_back(Move{10});
+    vec.push_back(Move{20});
+  */
   Move(const Move &&source) noexcept;
   //Destructor
   ~Move();
