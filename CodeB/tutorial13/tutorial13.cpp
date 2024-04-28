d0ec848d5e8912f19d901f2028704afef50677bf
MtJakhar <thvsd.10fingers@gmail.com>
2024-04-28 07:55:36 -0500

    tutorial 13

---
+++ CodeB/tutorial13/tutorial13.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+  int number, reversedNumber=0;
+  cout << "Enter Number : \n";
+  cin >> number;
+
+  while(number != 0) {
+    reversedNumber *= 10;
+    int numberToAdd = number % 10;
+    reversedNumber += numberToAdd;
+    number /= 10;
+  }
+
+  cout << "reversed: " << reversedNumber << endl;
+}
