2942fb0edc7644d7d8ec182fa016ff57408f2ed3
MtJakhar <thvsd.10fingers@gmail.com>
2024-04-28 07:42:33 -0500

    tut 12

---
+++ CodeB/tutorial12/tutorial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  //cout digits of a number
+  int number;
+  cout << "Number: ";
+  cin >> number;
+
+  if(number == 0) {
+    cout << "You have entered 0;\n";
+  } else {
+    int counter = 0;
+    while(number > 0) {
+      number /= 10;
+      counter++;
+    }
+    cout << "Number contains " << counter << " digits\n";
+  }
+}
