6a60955b85127aea4f545a423eef26e0eb16b6ff
MtJakhar <thvsd.10fingers@gmail.com>
2024-04-29 17:44:25 -0500

    recursion

---
+++ CodeB/functionsC++/recursion/tutorial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int recursive_sum(int m, int n) {
+  if(m == n) {
+    return m;
+  }
+  return m + recursive_sum(m + 1, n);
+}
+
+//sum number betwwen m-n
+int main () {
+  int m = 2, n = 4;
+  // int sum = 0;
+  
+  // for(int i = m; i <= n; i++) {
+  //   sum += i;
+  // }
+  cout << "sum = " << recursive_sum(m,n); 
+}
