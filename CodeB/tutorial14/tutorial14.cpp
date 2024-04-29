5748098dadd1f77c038967fbefb847bd218313b5
MtJakhar <thvsd.10fingers@gmail.com>
2024-04-29 10:34:11 -0500

    tutorial 14

---
+++ CodeB/tutorial14/tutorial14.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+  //do while loop
+  int userPin{1234}, pin, errorCounter{0};
+
+  do {
+    cout << "PIN: ";
+    cin >> pin;
+    if(pin != userPin) {
+      errorCounter++;
+    }
+  } while (pin != userPin && errorCounter < 3);
+  if(errorCounter < 3) {
+    cout << "Loading...";
+  } else {
+    cout << "Blocked...";
+  }
+
+}
