0123f52e0bd475e03f5ab82e42fa14c8a5514586
MtJakhar <thvsd.10fingers@gmail.com>
2024-04-30 10:29:47 -0500

    constructors

---
+++ CodeB/OOP/ClassesAndObjects/tutorial.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+//class is a user defined datatype
+/*class is like a car, while object would be a subset of a car like a volo, ford, ect*/
+
+class YouTubeChannel {
+  //attributes are private by default.
+  public:
+  string Name;
+  string OwnerName;
+  int SubscriberCount;
+  list<string> PublishedVideoTitles;
+
+};
+
+int main () {
+  YouTubeChannel ytChannel;
+  ytChannel.Name = "CodeBeauty";
+  ytChannel.OwnerName = "Saldina";
+  ytChannel.SubscriberCount = 1500;
+  ytChannel.PublishedVideoTitles = {"C++ for beginners Video1", "HTML & CSS Video1", "C+++ OOP Video1"};
+
+  cout << "Name: " << ytChannel.Name << endl;
+  cout << "OwnerName: " << ytChannel.OwnerName << endl;
+  cout << "SubscriberCount: " << ytChannel.SubscriberCount << endl;
+  cout << "PublishedVideoTitles: ";
+  for(string videoTitle : ytChannel.PublishedVideoTitles) {
+    cout << videoTitle << endl;
+  }
+}
---
+++ CodeB/OOP/ConstructorClassMethods/tutorial.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+/*
+constructors - has the same name as class, does not have return type
+*/
+
+class YouTubeChannel {
+  //attributes are private by default.
+  public:
+  string Name;
+  string OwnerName;
+  int SubscriberCount;
+  list<string> PublishedVideoTitles;
+
+  YouTubeChannel(string name, string ownerName) {
+    Name = name;
+    OwnerName = ownerName;
+    SubscriberCount = 0;
+  }
+
+  void GetInfo() {
+    cout << "Name: " << Name << endl;
+    cout << "OwnerName: " << OwnerName << endl;
+    cout << "SubscriberCount: " << SubscriberCount << endl;
+    cout << "PublishedVideoTitles: ";
+    for(string videoTitle : PublishedVideoTitles) {
+      cout << videoTitle << endl;
+    }
+  }
+
+
+
+};
+
+int main () {
+  YouTubeChannel ytChannel("CodeBeauty", "Saldina");
+  YouTubeChannel ytChannel2("AmySings" , "Amy");
+  // ytChannel.PublishedVideoTitles.push_back("C++ for beginners");
+  // ytChannel.PublishedVideoTitles.push_back("HTML & CSS for beginners");
+  // ytChannel.PublishedVideoTitles.push_back("C++ OOP for beginners");
+
+  // ytChannel2.PublishedVideoTitles.push_back("Apples and Bananas");
+
+  ytChannel.GetInfo();
+  ytChannel2.GetInfo();
+}
