18f86d470c6f8a496e8d3ceb277a99914dde82d7
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-03 11:41:19 -0500

    final code b

---
+++ CodeB/Pointers/funcpointercallbacks/tutorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+  callback/delegates
+
+  delegates- give you the ability to pass methods within other methods.
+
+  steps
+  -create a function of parent form
+  -pass that function to child with help of pointers
+  -store that function pointer on yur child form in a global variable
+  -invoke when you need it.
+
+*/
+
+int main () {
+  
+}
