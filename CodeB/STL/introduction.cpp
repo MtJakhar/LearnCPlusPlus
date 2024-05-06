03e5bf2a1b46672fba6e4fc9d3bc6dc6a2cc9e5c
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-06 10:42:21 -0500

    stl introduction

---
+++ CodeB/STL/introduction.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+/*
+  what is stl?
+  it is a library, that has tool which help us build applications
+
+  containers?
+  boxes in which we can store data that requires processing
+  The most commonly used containers are vector, list, map, set, stack, queue, etc;
+
+  Algorithms
+  set of instructions that help you perform certain tasks. such as processing the data within the containers.
+
+  function Objects (Functors)?
+  custom implementation of algorithms
+
+  Iterators- 
+  help for navigating through data inside containers
+
+  stl uses templates (standard template library).
+  
+  templates- 
+  generic code, which allow us to write a single piece of code that can work with different data types.
+*/
+
+int main () {
+  
+}
