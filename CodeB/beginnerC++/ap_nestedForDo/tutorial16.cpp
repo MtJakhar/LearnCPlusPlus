d3a65af24048178bc9e15efcfd5580728209e606
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-24 15:20:53 -0500

    nestedFordo review

--- CodeB/beginnerC++/ap_nestedForDo/tutorial16.cpp
+++ CodeB/beginnerC++/ap_nestedForDo/tutorial16.cpp
@@ -1,12 +1,27 @@
+// #include <iostream>
+
+// int main() {
+//   int grade, sum = 0; 
+//   for(int i = 0; i < 3; i++) {
+//     do{
+//       cout << "Enter grade " << i + 1 << ": "; 
+//       cin >> grade;
+//     } while (grade < 1 || grade > 5);
+//       sum += grade;
+//     }
+//   }
+//   cout << "Sum = " << sum << endl;
+//   cout << "Avg = " << (float)sum/3 << endl;
+// }
 #include <iostream>
 using namespace std;
 
-int main() {
-  int grade, sum=0; 
+int main () {
+  int grade, sum = 0;
   for(int i = 0; i < 3; i++) {
-    do{
-      cout << "Enter grade " << i + 1 << ": "; 
-      cin >> grade;
+    do {
+      cout << "enter grade " << i + 1 << ": ";
+      cin >> grade; 
     } while (grade < 1 || grade > 5);
     sum += grade;
   }
