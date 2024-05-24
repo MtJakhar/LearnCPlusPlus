7c5dc2fb15eaca8fd7f34494c95a882bc216e8a7
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-24 15:31:33 -0500

    nestedShapeMaker  review

--- CodeB/beginnerC++/ar_nestedForCreateShapes/tutorial_18.cpp
+++ CodeB/beginnerC++/ar_nestedForCreateShapes/tutorial_18.cpp
@@ -1,3 +1,27 @@
+// #include <iostream>
+// #include <iomanip>
+
+// using namespace std;
+
+// int main () {
+//   int width, height;
+//   cout<< "Height: ";
+//   cin >> height;
+//   cout<< "Width: ";
+//   cin >> width;
+
+//   char symbol;
+//   cout << "Symbol :";
+//   cin >> symbol;
+
+//   for(int h = 0; h < height; h++) {
+//     for(int w = 0; w < width; w++) {
+//       cout << setw(3) << symbol;
+//     }
+//     cout << endl;
+//   }
+// }
+
 #include <iostream>
 #include <iomanip>
 
@@ -5,13 +29,13 @@ using namespace std;
 
 int main () {
   int width, height;
-  cout<< "Height: ";
+  cout << "Height: ";
   cin >> height;
   cout<< "Width: ";
   cin >> width;
 
   char symbol;
-  cout << "Symbol :";
+  cout << "Symbol: ";
   cin >> symbol;
 
   for(int h = 0; h < height; h++) {
