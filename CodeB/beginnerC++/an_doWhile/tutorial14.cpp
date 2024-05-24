74245fbd1ac9364768f604fa0c09ec7ddece4369
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-24 15:04:14 -0500

    do while review

--- CodeB/beginnerC++/an_doWhile/tutorial14.cpp
+++ CodeB/beginnerC++/an_doWhile/tutorial14.cpp
@@ -1,12 +1,30 @@
+// #include <iostream>
+// using namespace std;
+
+// int main () {
+//   //do while loop
+//   int userPin{1234}, pin, errorCounter{0};
+
+//   do {
+//     cout << "PIN: ";
+//     cin >> pin;
+//     if(pin != userPin) {
+//       errorCounter++;
+//     }
+//   } while (pin != userPin && errorCounter < 3);
+//   if(errorCounter < 3) {
+//     cout << "Loading...";
+//   } else {
+//     cout << "Blocked...";
+//   }
+// }
 #include <iostream>
 using namespace std;
-
 int main () {
-  //do while loop
   int userPin{1234}, pin, errorCounter{0};
 
-  do {
-    cout << "PIN: ";
+  do{
+    cout << "Pin: ";
     cin >> pin;
     if(pin != userPin) {
       errorCounter++;
@@ -17,5 +35,4 @@ int main () {
   } else {
     cout << "Blocked...";
   }
-
 }
