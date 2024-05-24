1ed1f2e03df40554ffef7f3b4a967f34698cb83c
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-24 14:13:43 -0500

    bmi review

--- CodeB/beginnerC++/ag_bmiCal/tutorial7.cpp
+++ CodeB/beginnerC++/ag_bmiCal/tutorial7.cpp
@@ -1,25 +1,49 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main () {
+//   //BMI calculator
+//   //weight/height * height
+//   float weight, height, bmi;
+
+//   cout << "what is your weight(kg)\n";
+//   cin >> weight;
+//   cout << "what is your height(cm)\n";
+//   cin >> height;
+
+//   bmi = weight/(height * height);
+
+//   if(bmi < 18.5) {
+//     cout << "\nunderweight\n";
+//   } else if (bmi > 25) {
+//       cout << "\noverweight\n";
+//   } else {
+//     cout << "\nnormal\n";
+//   }
+//   cout << "\nYour bmi is " << bmi;
+// }
+
 #include <iostream>
 
 using namespace std;
 
 int main () {
-  //BMI calculator
-  //weight/height * height
   float weight, height, bmi;
-
-  cout << "what is your weight(kg)\n";
+  
+  cout << "what is your wight(kg)\n";
   cin >> weight;
-  cout << "what is your height(kg)\n";
+  cout << "what is your height(cm)\n";
   cin >> height;
 
   bmi = weight/(height * height);
 
   if(bmi < 18.5) {
-    cout << "\nunderweight\n";
-  } else if (bmi > 25) {
-      cout << "\noverweight\n";
+    cout << "you are underweight\n";
+  } else if( bmi > 25) {
+    cout << "you are overweight\n";
   } else {
-    cout << "\nnormal\n";
+    cout << "normal\n";
   }
-  cout << "\nYour bmi is " << bmi;
 }
+
