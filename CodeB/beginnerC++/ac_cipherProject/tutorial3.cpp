d71b05a2c3cdf0f4b33649b2b3df3d9463d1d6f7
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-24 13:22:04 -0500

    tut3 beginner review

--- CodeB/beginnerC++/ac_cipherProject/tutorial3.cpp
+++ CodeB/beginnerC++/ac_cipherProject/tutorial3.cpp
@@ -1,26 +1,49 @@
 /*
 
 */
+
+
+// int main () {
+//   //below are casting operators  both lines do the same thing
+//   // cout << (int)'a' << endl;
+//   // cout << int('a') << endl;
+//   // cout << int('A') << endl;
+//   // cout << char(65) << endl;
+
+//   //you can create multiple variables at once if the are of same type.
+//   char c1, c2, c3, c4, c5;
+//   cout << "enter 5 letters\n";
+//   //you can enter multiple variables with binding.
+//   cin >> c1 >> c2 >> c3 >> c4 >> c5;
+//   cout << "ACSII message: " << int(c1) << " " << int(c2) << " " << int(c3) << " " << int(c4) << " " << int(c5) << endl;
+
+//   int i1, i2, i3, i4, i5;
+//   cout << "Enter ascii cipher of 5 integers\n";
+//   cin >> i1 >> i2 >> i3 >> i4 >> i5;
+//   cout << "Here is your message: " << char(i1) << char(i2) << char(i3) << char(i4) << char(i5); 
+
+// }
+
 #include <iostream>
+
 using namespace std;
 
 int main () {
-  //below are casting operators  both lines do the same thing
-  // cout << (int)'a' << endl;
-  // cout << int('a') << endl;
-  // cout << int('A') << endl;
-  // cout << char(65) << endl;
+  //below are casting operators 
+  cout << int('a') << endl;
+  cout << int('a') << endl;
+  cout << int('A') << endl;
+  cout << char(65) << endl;
 
-  //you can create multiple variables at once if the are of same type.
   char c1, c2, c3, c4, c5;
-  cout << "enter 5 letters\n";
-  //you can enter multiple variables with binding.
+  cout << "enter 5 letters";
   cin >> c1 >> c2 >> c3 >> c4 >> c5;
-  cout << "ACSII message: " << int(c1) << " " << int(c2) << " " << int(c3) << " " << int(c4) << " " << int(c5) << endl;
-
+  cout << "ASCII message: " << int(c1) << " " << int(c2) << " "
+ << int(c3) << " " << int(c4) << " " << int(c5) << endl;
   int i1, i2, i3, i4, i5;
-  cout << "Enter ascii cipher of 5 integers\n";
+  cout << "Enter ascii cipher of 5 integers \n";
   cin >> i1 >> i2 >> i3 >> i4 >> i5;
-  cout << "Here is your message: " << char(i1) << char(i2) << char(i3) << char(i4) << char(i5); 
+  cout << "here is your message: " << char(i1) << char(i2) << char(i3) << char(i4) << char(i5);
 
-}
+  
+ }
