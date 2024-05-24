07b11563a7ace6ab76981c6896c4ce8c27cf6a57
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-24 14:58:49 -0500

    whileReverse Number

--- CodeB/beginnerC++/am_whileloopReverseNumber/tutorial13.cpp
+++ CodeB/beginnerC++/am_whileloopReverseNumber/tutorial13.cpp
@@ -1,18 +1,34 @@
-#include <iostream>
+// #include <iostream>
 
+// using namespace std;
+
+// int main () {
+//   int number, reversedNumber=0;
+//   cout << "Enter Number : \n";
+//   cin >> number;
+
+//   while(number != 0) {
+//     reversedNumber *= 10;
+//     int numberToAdd = number % 10;
+//     reversedNumber += numberToAdd;
+//     number /= 10;
+//   }
+
+//   cout << "reversed: " << reversedNumber << endl;
+// }
+#include <iostream>
 using namespace std;
 
 int main () {
-  int number, reversedNumber=0;
-  cout << "Enter Number : \n";
+  int number , reversedNumber = 0;
+  cout << "Enter Number: \n";
   cin >> number;
 
-  while(number != 0) {
+  while(number !=0 ) {
     reversedNumber *= 10;
     int numberToAdd = number % 10;
     reversedNumber += numberToAdd;
     number /= 10;
   }
-
   cout << "reversed: " << reversedNumber << endl;
 }
