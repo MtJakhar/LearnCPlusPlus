108446776dd120a0ba26478cb9e51e01918989fa
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-24 14:50:21 -0500

    whileifelse review

--- CodeB/beginnerC++/al_whileAndIfElse/tutorial.cpp
+++ CodeB/beginnerC++/al_whileAndIfElse/tutorial.cpp
@@ -1,21 +1,41 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main() {
+//   //cout digits of a number
+//   int number;
+//   cout << "Number: ";
+//   cin >> number;
+
+//   if(number == 0) {
+//     cout << "You have entered 0;\n";
+//   } else {
+//     int counter = 0;
+//     while(number > 0) {
+//       number /= 10;
+//       counter++;
+//     }
+//     cout << "Number contains " << counter << " digits\n";
+//   }
+// }
 #include <iostream>
 
 using namespace std;
 
 int main() {
-  //cout digits of a number
   int number;
-  cout << "Number: ";
+  cout << "Number" << endl;
   cin >> number;
 
   if(number == 0) {
-    cout << "You have entered 0;\n";
+    cout << "0 digits";
   } else {
     int counter = 0;
-    while(number > 0) {
+    while(number > 10) {
       number /= 10;
       counter++;
     }
-    cout << "Number contains " << counter << " digits\n";
+    cout << counter << "digits" << endl;
   }
 }
