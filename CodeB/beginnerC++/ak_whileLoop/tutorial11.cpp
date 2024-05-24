80883146fecbc5551a66bcedf53e4d577d3c3e77
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-24 14:45:46 -0500

    whileloop review

--- CodeB/beginnerC++/ak_whileLoop/tutorial11.cpp
+++ CodeB/beginnerC++/ak_whileLoop/tutorial11.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
+// #include <iostream>
 
+// using namespace std;
+
+// int main () {
+//   int counter = 100;
+//   while(counter <= 500) {
+//     if(counter % 3 == 0 && counter % 5 == 0) {
+//       cout << counter << endl;
+//     }
+//     counter++;
+//   }
+// }
+
+#include <iostream>
 using namespace std;
 
 int main () {
