c50179ca4faa088fc1cf1fe739e28c5148039ba8
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-24 15:34:51 -0500

    nested Loops again review

--- CodeB/beginnerC++/as_NestedLoopsAgain/tutorial19.cpp
+++ CodeB/beginnerC++/as_NestedLoopsAgain/tutorial19.cpp
@@ -3,21 +3,40 @@
 
 using namespace std;
 
+// int main () {
+//   int length;
+//   cout << "length: ";
+//   cin >> length;
+
+//   char symbol;
+//   cout << "Symbol: ";
+//   cin >> symbol;
+
+//   // for(int i = 1; i <= length; i++) {
+//   //   for(int j = 1; j <= i; j++) {
+//   //     cout << setw(3) << symbol;
+//   //   }
+//   //   cout << endl;
+//   // }
+
+//   for(int i = length; i >= 1; i--) {
+//     for(int j = 1; j <= i; j++) {
+//       cout << setw(3) << symbol;
+//     }
+//     cout << endl;
+//   }
+
+  
+// }
+
 int main () {
   int length;
   cout << "length: ";
   cin >> length;
 
   char symbol;
-  cout << "Symbol: ";
+  cout << "Symbol";
   cin >> symbol;
-
-  // for(int i = 1; i <= length; i++) {
-  //   for(int j = 1; j <= i; j++) {
-  //     cout << setw(3) << symbol;
-  //   }
-  //   cout << endl;
-  // }
 
   for(int i = length; i >= 1; i--) {
     for(int j = 1; j <= i; j++) {
@@ -25,6 +44,4 @@ int main () {
     }
     cout << endl;
   }
-
-  
 }
