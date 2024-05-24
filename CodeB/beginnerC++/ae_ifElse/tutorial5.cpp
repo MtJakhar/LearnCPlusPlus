95a714b495578243e4edf91bb7de5bc30fc14b5d
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-24 13:32:33 -0500

    if-else review

--- CodeB/beginnerC++/ae_ifElse/tutorial5.cpp
+++ CodeB/beginnerC++/ae_ifElse/tutorial5.cpp
@@ -1,20 +1,40 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//   //enter sides of tri a,b,c
+//   //program should write out whether that tri is quil,isos,scal.
+//   float a, b, c;
+//   cout << "a, b, c" << endl;
+//   cin >> a >> b >> c;
+  
+//   if(a==b && b == c) {
+//     cout << "equal" << endl;
+//   }  else {
+//     if (a!= b && a!=c && b != c ) {
+//     cout << "scalene" << endl;
+//     } else {
+//       cout << "iso" << endl;
+//     }
+//   }
+// }
+
 #include <iostream>
+
 using namespace std;
 
-int main() {
-  //enter sides of tri a,b,c
-  //program should write out whether that tri is quil,isos,scal.
+int main () {
   float a, b, c;
-  cout << "a, b, c" << endl;
+  cout << "enter three numbers, which will be sides of triangle \n";
   cin >> a >> b >> c;
-  
-  if(a==b && b == c) {
-    cout << "equal" << endl;
-  }  else {
-    if (a!= b && a!=c && b != c ) {
-    cout << "scalene" << endl;
+
+  if(a==b && b==c) {
+    cout << "equal triangle" << endl;
+  } else {
+    if(!a==b && !b==c) {
+      cout << "scalene triangle" << endl;
     } else {
-      cout << "iso" << endl;
+      cout << "iso triangle" << endl;
     }
   }
 }
