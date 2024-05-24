f93d87ce666201b3753c7178741743cd460f5a24
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-24 15:23:52 -0500

    nestedFor review

--- CodeB/beginnerC++/aq_nestedFor/tutorial17.cpp
+++ CodeB/beginnerC++/aq_nestedFor/tutorial17.cpp
@@ -1,12 +1,24 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main () {
+//   for(int i = 1; i <= 10; i++) {
+//     for(int i2 = 1; i2 <= 10; i2++) {
+//       cout << i << " * "  << i2 << " = " << i * i2 << endl;
+//     }
+//     cout << endl;
+//   }
+// }
+
 #include <iostream>
 
 using namespace std;
 
 int main () {
   for(int i = 1; i <= 10; i++) {
-    for(int i2 = 1; i2 <= 10; i2 ++) {
-      cout << i << " * "  << i2 << " = " << i * i2 << endl;
+    for(int i2 = 1; i2 <= 10; i2++) {
+      cout << i << " * " << i2 << " = " << i * i2 << endl;
     }
-    cout << endl;
   }
 }
