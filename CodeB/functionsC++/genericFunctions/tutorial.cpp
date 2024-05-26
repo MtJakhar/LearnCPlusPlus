048717ff46fea67fc185bcd0223c2d297cc77697
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-26 17:29:05 -0500

    creating genericFunction and templates review

--- CodeB/functionsC++/genericFunctions/tutorial.cpp
+++ CodeB/functionsC++/genericFunctions/tutorial.cpp
@@ -1,8 +1,34 @@
+// #include <iostream>
+
+// using namespace std;
+
+// //template allows you to create functions that can use any datatype
+// template<typename T>
+
+// void Swap(T& a, T& b) {
+//   //the reference operator in this case points to the memory location of T and changes it directly in the memory instead of duplicating the value in another address. 
+//   T temp = a;
+//   a = b;
+//   b = temp;
+// }
+
+// int main () {
+//   int a = 5, b = 7;
+//   cout << a << " - " << b << endl;
+//   Swap(a,b);
+//   cout << a << " - " << b << endl;
+
+//   char c = 'c', d = 'd';
+//   cout << c << " - " << d << endl;
+//   Swap(c,d);
+//   cout << c << " - " << d << endl;
+// }
+
 #include <iostream>
 
 using namespace std;
 
-template<typename T>
+template <typename T>
 
 void Swap(T& a, T& b) {
   T temp = a;
@@ -12,15 +38,14 @@ void Swap(T& a, T& b) {
 
 int main () {
   int a = 5, b = 7;
-  cout << a << " - " << b << endl;
+  cout << a <<  " - " << b << endl;
   Swap(a,b);
   cout << a << " - " << b << endl;
-
-  char c = 'c', d = 'd';
+  
+  char c ='c', d = 'd';
   cout << c << " - " << d << endl;
-  Swap(c,d);
+  swap(c,d);
   cout << c << " - " << d << endl;
-
 }
 
 
