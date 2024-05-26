5c6fca607917a9a52c3564b6a11f69e1ea403c6d
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-26 18:04:44 -0500

    return type review

--- CodeB/functionsC++/returnTypes/tutorial.cpp
+++ CodeB/functionsC++/returnTypes/tutorial.cpp
@@ -1,12 +1,40 @@
+// #include <iostream>
+
+// using namespace std;
+
+// bool isPrimeNumber(int number);
+
+// int main () {
+//   int number;
+//   cout << "Number : ";
+//   cin >> number;
+//   bool isPrimeFlag = isPrimeNumber(number);
+
+//   if(isPrimeFlag) {
+//     cout << "Prime number" << endl;
+//   } else {
+//     cout << "Not Prime number" << endl;
+//   }
+// }
+
+// bool isPrimeNumber(int number) {
+//   for(int i = 2; i < number; i ++) {
+//     if(number%i == 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// };
+
 #include <iostream>
 
 using namespace std;
 
 bool isPrimeNumber(int number);
 
-int main () {
+int main() {
   int number;
-  cout << "Number : ";
+  cout << "Number: ";
   cin >> number;
   bool isPrimeFlag = isPrimeNumber(number);
 
@@ -17,11 +45,13 @@ int main () {
   }
 }
 
-bool isPrimeNumber(int number) {
-  for(int i = 2; i < number; i ++) {
+bool isPrimeFlag(int number) {
+  for(int i = 2; i < number; i++) {
     if(number%i == 0) {
       return false;
     }
   }
   return true;
-};
+}
+
+
