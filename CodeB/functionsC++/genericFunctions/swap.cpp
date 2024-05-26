a89be496157b4f9e9f9675cd42c9017a013a2b89
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-26 17:07:53 -0500

    switching variables review

--- CodeB/functionsC++/genericFunctions/swap.cpp
+++ CodeB/functionsC++/genericFunctions/swap.cpp
@@ -1,24 +1,41 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main () {
+//   //swapping variables
+//   int a = 20;
+//   int b = 10;
+//   int temp;
+
+//   // temp = a;
+//   // a = b;
+//   // b = a;
+
+//   a = a + b;
+//   b = a - b;
+//   a = a - b;
+
+//   cout << " a = " << a << endl;
+//   cout << "b = " << b;
+// }
+
 #include <iostream>
 
 using namespace std;
 
-
-
 int main () {
-  //swapping variables
+
   int a = 20;
-  int b = 10;
+  int b = 11;
   int temp;
 
-  // temp = a;
-  // a = b;
-  // b = a;
+  temp = b;
+  b = a;
+  a = b;
 
   a = a + b;
   b = a - b;
   a = a - b;
-
-  cout << " a = " << a << endl;
-  cout << "b = " << b;
 }
 
