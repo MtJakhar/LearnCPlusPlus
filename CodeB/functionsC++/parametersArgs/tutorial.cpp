71220f05bba91462efb498dc400be99df0b64f88
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-26 17:50:31 -0500

    parameters review

--- CodeB/functionsC++/parametersArgs/tutorial.cpp
+++ CodeB/functionsC++/parametersArgs/tutorial.cpp
@@ -1,8 +1,30 @@
+// #include <iostream>
+
+// using namespace std;
+// //default values must be at end with other default values.
+// void introduceMe(string name, string city, int age=0);
+
+// int main () {
+//   string name, city;
+//   int age;
+//   cout << "name, city, age, enter in that order\n";
+//   cin >> name >> city >> age;
+//   introduceMe(name, city, age);
+// }
+
+// void introduceMe(string name, string city, int age) {
+//   cout << "My name is " << name << endl;
+//   cout << "My city is " << city << endl;
+//   if(age !=0) {
+//     cout << "My age is " << age << endl;
+//   }
+// }
+
 #include <iostream>
 
 using namespace std;
-//default values must be at end with other default values.
-void introduceMe(string name, string city, int age=0);
+
+void introduceMe(string name, string city, int age = 0);
 
 int main () {
   string name, city;
@@ -10,15 +32,13 @@ int main () {
   cout << "name, city, age, enter in that order\n";
   cin >> name >> city >> age;
   introduceMe(name, city, age);
-  // introduceMe("MR.APPLE", "hemet", 34);
-  // introduceMe("Tamoor", "san diego", 18);
-  // introduceMe("aayra", "san diego");
+
 }
 
 void introduceMe(string name, string city, int age) {
-  cout << "My name is " << name << endl;
+  cout << "MY name is " << name << endl;
   cout << "My city is " << city << endl;
-  if(age !=0) {
+  if(age != 0) {
     cout << "My age is " << age << endl;
   }
 }
