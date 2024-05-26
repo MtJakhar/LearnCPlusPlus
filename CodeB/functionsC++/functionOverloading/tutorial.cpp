62586ca59f86ea4fdb1bdf175d248e3331d15bf7
MtJakhar <thvsd.10fingers@gmail.com>
2024-05-26 17:02:56 -0500

    function overloading review

--- CodeB/functionsC++/functionOverloading/tutorial.cpp
+++ CodeB/functionsC++/functionOverloading/tutorial.cpp
@@ -1,25 +1,52 @@
+// #include <iostream>
+
+// using namespace std;
+
+// //function overloading is a feature that allows you to define multiple functions with the same name but different parameters and or return types.
+// int sum(int a, int b);
+// double sum(double a , double b);
+// float sum(float a, float b, float c);
+
+// int main () {
+//   cout << sum(4 ,3) << endl;
+//   cout << sum(4.4 ,3.3) << endl;
+//   cout << sum(4.4 ,3.3, 2.2) << endl;
+// }
+
+// int sum(int a, int b) {
+//   return  a + b;
+// }
+
+// double sum(double a, double b) {
+//   return a + b;
+// }
+
+// float sum(float a, float b, float c) {
+//   return a + b + c;
+// }
+
 #include <iostream>
 
 using namespace std;
 
 int sum(int a, int b);
-double sum(double a , double b);
+double sum(double a, double b);
 float sum(float a, float b, float c);
 
 int main () {
-  cout << sum(4 ,3) << endl;
-  cout << sum(4.4 ,3.3) << endl;
-  cout << sum(4.4 ,3.3, 2.2) << endl;
+  cout << sum(4, 3) << endl;
+  cout << sum(4.4, 3.3) << endl;
+  cout << sum(4.4, 3.3, 2.2) << endl;
 }
 
-int sum(int a, int b) {
-  return  a + b;
-}
-
-double sum(double a, double b) {
+int sum (int a, int b) {
   return a + b;
 }
 
-float sum(float a, float b, float c) {
+double sum (double a, double b) {
+  return a + b;
+}
+
+float sum (float a, float b, float c) {
   return a + b + c;
 }
