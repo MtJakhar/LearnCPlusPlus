8a6f5f1a26a2f13ad63ab1ea0bd2343856e298c4
MtJakhar <thvsd.10fingers@gmail.com>
2024-07-28 14:26:29 -0400

    swapfunction update

--- CodeB/functionsC++/genericFunctions/tutorial.cpp
+++ CodeB/functionsC++/genericFunctions/tutorial.cpp
@@ -1,52 +1,26 @@
-// #include <iostream>
-
-// using namespace std;
-
-// //template allows you to create functions that can use any datatype
-// template<typename T>
-
-// void Swap(T& a, T& b) {
-//   //the reference operator in this case points to the memory location of T and changes it directly in the memory instead of duplicating the value in another address. 
-//   T temp = a;
-//   a = b;
-//   b = temp;
-// }
-
-// int main () {
-//   int a = 5, b = 7;
-//   cout << a << " - " << b << endl;
-//   Swap(a,b);
-//   cout << a << " - " << b << endl;
-
-//   char c = 'c', d = 'd';
-//   cout << c << " - " << d << endl;
-//   Swap(c,d);
-//   cout << c << " - " << d << endl;
-// }
-
 #include <iostream>
 
 using namespace std;
 
-template <typename T>
-
-void Swap(T& a, T& b) {
-  T temp = a;
-  a = b;
-  b = temp;
+// Template function to swap two values
+template<typename Type>
+void swapFunction(Type& a, Type& b) {
+    Type temp = a;
+    a = b;
+    b = temp;
 }
 
-int main () {
-  int a = 5, b = 7;
-  cout << a <<  " - " << b << endl;
-  Swap(a,b);
-  cout << a << " - " << b << endl;
-  
-  char c ='c', d = 'd';
-  cout << c << " - " << d << endl;
-  swap(c,d);
-  cout << c << " - " << d << endl;
+int main() {
+  int alpha = 1, beta = 2;
+  cout << "alpha: " << alpha << " - " << "beta: " << beta << endl;
+  swapFunction(alpha, beta);
+  cout << "alpha: " << alpha << " - " << "beta: " << beta << endl;
+
+  char c = 'c', d = 'd';
+  cout << "c: " << c << "\n" << "d: " << d << endl;
+  swapFunction(c, d);
+  cout << "c: " << c << "\n" << "d: " << d << endl;
+
+  return 0;
 }
-
-
 
