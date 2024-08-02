785db14573ec370dc18a53734ed67d26030f797e
MtJakhar <thvsd.10fingers@gmail.com>
2024-08-02 13:20:22 -0400

    classes

--- CodeB/OOP/ClassesAndObjects/tutorial.cpp
+++ CodeB/OOP/ClassesAndObjects/tutorial.cpp
@@ -1,60 +1,31 @@
-// #include <iostream>
-// #include <list>
-
-// using namespace std;
-// //class is a user defined datatype
-// /*class is like a car, while object would be a subset of a car like a volo, ford, ect*/
-
-// class YouTubeChannel {
-//   //attributes are private by default.
-//   public:
-//   string Name;
-//   string OwnerName;
-//   int SubscriberCount;
-//   list<string> PublishedVideoTitles;
-
-// };
-
-// int main () {
-//   YouTubeChannel ytChannel;
-//   ytChannel.Name = "CodeBeauty";
-//   ytChannel.OwnerName = "Saldina";
-//   ytChannel.SubscriberCount = 1500;
-//   ytChannel.PublishedVideoTitles = {"C++ for beginners Video1", "HTML & CSS Video1", "C+++ OOP Video1"};
-
-//   cout << "Name: " << ytChannel.Name << endl;
-//   cout << "OwnerName: " << ytChannel.OwnerName << endl;
-//   cout << "SubscriberCount: " << ytChannel.SubscriberCount << endl;
-//   cout << "PublishedVideoTitles: ";
-//   for(string videoTitle : ytChannel.PublishedVideoTitles) {
-//     cout << videoTitle << endl;
-//   }
-// }
 #include <iostream>
 #include <list>
+#include <string>
 
 using namespace std;
 
-class YouTubeChannel {
-  public:
+class YoutubeChannel {
+public:
   string Name;
   string OwnerName;
   int SubscriberCount;
-  list<string> PublishedVideoTitles;
+  list<string>PublishedVideos;
 };
 
 int main () {
-  YouTubeChannel ytChannel;
-  ytChannel.Name = "Aayra's Playground";
-  ytChannel.OwnerName = "Aayra";
-  ytChannel.SubscriberCount = 1500;
-  ytChannel.PublishedVideoTitles = { "first", "second", "thrid" };
+  YoutubeChannel ytChannel;
+  ytChannel.Name = "BestChannel";
+  ytChannel.OwnerName = "jack";
+  ytChannel.SubscriberCount = 15;
+  ytChannel.PublishedVideos = {"vid 1", "vid 2", "vid 3"};
 
-  cout << "Name: " << ytChannel.Name << endl;
-  cout << "OwnerName: " << ytChannel.OwnerName << endl;
-  cout << "SubscriberCount " << ytChannel.SubscriberCount << endl;
-  cout << "Published videos";
-  for(string title : ytChannel.PublishedVideoTitles) {
-    cout << title << endl; 
+  cout << "channel name: "<<ytChannel.Name << endl;
+  cout << "owner name: " << ytChannel.OwnerName << endl;
+  cout << "sub: " << ytChannel.SubscriberCount<< endl;
+  cout << "videos: " << endl;
+  for(string vid: ytChannel.PublishedVideos) {
+    cout  << vid << endl;
   }
+
+  return 0;
 }
